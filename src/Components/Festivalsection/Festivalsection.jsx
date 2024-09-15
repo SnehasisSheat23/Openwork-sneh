c2463ce835bb826b7b5a6bb25214857d076cb174
Rupam <rupammondal564@gmail.com>
2024-09-15 17:58:34 +0530

    Update

--- src/Components/Festivalsection/Festivalsection.jsx
+++ src/Components/Festivalsection/Festivalsection.jsx
@@ -17,7 +17,7 @@ function Festivalsection() {
                 </div>
 
 
-                <div className="w-full px-20 flex gap-[3vw] box-border justify-between mt-[10vh]">
+                <div className="w-full px-20 flex gap-[2vw] box-border justify-between mt-[10vh]">
                     <Card img={"https://prismic-assets-cdn.tomorrowland.com/Zi98lN3JpQ5PTPLa_Zeb4NeXgT-BdbvEx_Brasil.jpg?crop=6991%2C5243%2C435%2C0&width=352&height=264"} text={"Gaeba Dhammal Canada"} />
                     <Card img={"https://prismic-assets-cdn.tomorrowland.com/ZrIItkaF0TcGItrp_AFTERLIFE_AMS___06_1920x1080.jpg?crop=1440%2C1080%2C240%2C0&width=352&height=264"} text={"India fest live in schamburg"} />
                     <Card img={"https://prismic-assets-cdn.tomorrowland.com/Zg5XLzskWekewC1C_OS24_001_OURSTORY_XXYEARS_KEYVISUAL_EYE_WEBSITE_HEADER.jpg?crop=2133%2C1600%2C133%2C0&width=352&height=264"} text={"Chicago Garba Fest Renaissance"} />
