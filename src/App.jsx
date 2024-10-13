2671894420a43a6ac72c651099edbdc68bac9ddb
Snehasis Shit <Snehasisshit2326@gmail.com>
2024-10-14 01:06:57 +0530

    fixed fav icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import LocomotiveScroll from 'locomotive-scroll';
 import './App.css'
 import React from 'react';
 import Routing from "./Components/Routes/Routing";
+import Favicon from 'react-favicon';
 
 function App(){
   const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
+      <Favicon url="/image.svg" />
       <Routing/>
     </>
   )
